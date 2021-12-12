77deb9129036871db0d1c659a6e8c7082ecc0d2c
ishwar46 <ishwarura@gmail.com>
2021-12-12 11:13:03 +0545

    Added

--- Day4/day4lab4.py
+++ Day4/day4lab4.py
@@ -4,6 +4,7 @@ INPUT: 1        OUTPUT: 1
 
 INPUT: -1        OUTPUT: 1"""
 
-print("Enter a number")
-number = int(input(""))
-number
+n=int(input('enter a number:'))
+m=int(input('enter second number:'))
+print("the absolute value of the give number is:", abs(n))
+print("the absolute value of the given number is:", abs(m))
